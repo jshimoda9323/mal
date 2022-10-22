b20e27d5b44bf9424b9f3bcf4746270c64e081a3
Jeff Shimoda <jshimoda@ca.ibm.com>
2022-10-22 02:26:34 -0400

    code cleanup

--- impls/rust.2/step3_env.rs
+++ impls/rust.2/step3_env.rs
@@ -24,16 +24,12 @@ fn eval_ast(mt: &MalType, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
             let mut new_str_dict = HashMap::<String, MalType>::new();
             let mut new_key_dict = HashMap::<String, MalType>::new();
             for (key, val) in str_dict.iter() {
-                match eval(val, repl_env) {
-                    Ok(result) => { let _ = new_str_dict.insert(key.to_string(), result); }
-                    Err(malerr) => return Err(malerr)
-                }
+                let result = eval(val, repl_env)?;
+                let _ = new_str_dict.insert(key.to_string(), result);
             }
             for (key, val) in key_dict.iter() {
-                match eval(val, repl_env) {
-                    Ok(result) => { let _ = new_key_dict.insert(key.to_string(), result); }
-                    Err(malerr) => return Err(malerr)
-                }
+                let result = eval(val, repl_env)?;
+                let _ = new_key_dict.insert(key.to_string(), result);
             }
             Ok(MalType::Dictionary(new_str_dict, new_key_dict))
         }
@@ -41,10 +37,8 @@ fn eval_ast(mt: &MalType, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
         MalType::List(list) => {
             let mut new_list: Vec<MalType> = Vec::new();
             for sexpr in list {
-                match eval(sexpr, repl_env) {
-                    Ok(result) => new_list.push(result),
-                    Err(malerr) => return Err(malerr)
-                }
+                let result = eval(sexpr, repl_env)?;
+                new_list.push(result);
             }
             Ok(MalType::List(new_list))
         }
@@ -52,19 +46,12 @@ fn eval_ast(mt: &MalType, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
         MalType::Number(n) => Ok(MalType::Number(*n)),
         MalType::Operator(op) => Ok(MalType::Operator(*op)),
         MalType::Str(s) => Ok(MalType::Str(s.to_string())),
-        MalType::Symbol(sym) => {
-            match repl_env.get(sym) {
-                Some(value) => Ok(value),
-                None => Err(MalErr::SymbolErr1(sym.to_string()))
-            }
-        }
+        MalType::Symbol(sym) => repl_env.get(sym).ok_or(MalErr::SymbolErr1(sym.to_string())),
         MalType::Vector(vec) => {
             let mut new_vec: Vec<MalType> = Vec::new();
             for sexpr in vec {
-                match eval(sexpr, repl_env) {
-                    Ok(result) => new_vec.push(result),
-                    Err(malerr) => return Err(malerr)
-                }
+                let result = eval(sexpr, repl_env)?;
+                new_vec.push(result);
             }
             Ok(MalType::Vector(new_vec))
         }
@@ -72,129 +59,75 @@ fn eval_ast(mt: &MalType, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
 }
 
 fn apply(op: &MalType, arg1: &MalType, arg2: &MalType) -> Result<MalType, MalErr> {
-    match op {
-        MalType::Operator(f) => match arg1 {
-            MalType::Number(n1) => match arg2 {
-                MalType::Number(n2) => Ok(MalType::Number(f(*n1, *n2))),
-                _ => Err(MalErr::TypeErr1("number".to_string(), arg2.prt_type().to_string(), "argument 2".to_string()))
-            }
-            _ => Err(MalErr::TypeErr1("number".to_string(), arg2.prt_type().to_string(), "argument 1".to_string()))
+    let f = if let MalType::Operator(f1) = op { f1 } else { return Err(MalErr::TypeErr1("operator".to_string(), arg2.prt_type().to_string(), "a function call".to_string())) };
+    let n1 = if let MalType::Number(n) = arg1 { n } else { return Err(MalErr::TypeErr1("number".to_string(), arg2.prt_type().to_string(), "argument 1".to_string())) };
+    let n2 = if let MalType::Number(n) = arg2 { n } else { return Err(MalErr::TypeErr1("number".to_string(), arg2.prt_type().to_string(), "argument 2".to_string())) };
+    Ok(MalType::Number(f(*n1, *n2)))
+}
+
+fn handle_let_bindings_internal(list: &Vec<MalType>, repl_env: &mut MalEnv) -> Result<bool, MalErr> {
+    if list.len() % 2 == 0 {
+        for pair in list.chunks(2) {
+            let result = eval(&pair[1], repl_env)?;
+            let sym = if let MalType::Symbol(s) = &pair[0] { s } else { return Err(MalErr::TypeErr1("symbol".to_string(),pair[0].prt_type().to_string(),"let* binding".to_string())) };
+            repl_env.set(sym, result);
         }
-        _ => Err(MalErr::TypeErr1("operator".to_string(), arg2.prt_type().to_string(), "a function call".to_string()))
+        Ok(true)
+    } else {
+        Err(MalErr::ElementErr1("an even number of list elements".to_string(), format!("{} list elements", list.len()).to_string()))
     }
 }
 
 fn handle_let_bindings(mt: &MalType, repl_env: &mut MalEnv) -> Result<bool, MalErr> {
     match mt {
-        MalType::List(list) => {
-            match list.len() % 2 {
-                0 => {
-                    for pair in list.chunks(2) {
-                        match eval(&pair[1], repl_env) {
-                            Ok(result) => {
-                                match &pair[0] {
-                                    MalType::Symbol(sym) => {
-                                        repl_env.set(sym, result);
-                                    }
-                                    _ => return Err(MalErr::TypeErr1("symbol".to_string(),pair[0].prt_type().to_string(),"let* binding".to_string()))
-                                }
-                            }
-                            Err(malerr) => return Err(malerr)
-                        }
-                    }
-                }
-                _ => return Err(MalErr::ElementErr1("an even number of list elements".to_string(), format!("{} list elements", list.len()).to_string()))
-            }
-        }
-        MalType::Vector(vec) => {
-            match vec.len() % 2 {
-                0 => {
-                    for pair in vec.chunks(2) {
-                        match eval(&pair[1], repl_env) {
-                            Ok(result) => {
-                                match &pair[0] {
-                                    MalType::Symbol(sym) => {
-                                        repl_env.set(sym, result);
-                                    }
-                                    _ => return Err(MalErr::TypeErr1("symbol".to_string(),pair[0].prt_type().to_string(),"let* binding".to_string()))
-                                }
-                            }
-                            Err(malerr) => return Err(malerr)
-                        }
-                    }
-                }
-                _ => return Err(MalErr::ElementErr1("an even number of list elements".to_string(), format!("{} list elements", vec.len()).to_string()))
-            }
-        }
-        _ => return Err(MalErr::TypeErr1("list or vector".to_string(), mt.prt_type().to_string(), "let* binding list".to_string()))
+        MalType::List(list) => handle_let_bindings_internal(list, repl_env),
+        MalType::Vector(vec) => handle_let_bindings_internal(vec, repl_env),
+        _ => Err(MalErr::TypeErr1("list or vector".to_string(), mt.prt_type().to_string(), "let* binding list".to_string()))
     }
-    Ok(true)
+}
+
+fn eval_def(list: &Vec<MalType>, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
+    if list.len() != 3 { return Err(MalErr::ElementErr1("2 arguments for def!".to_string(), format!("{} argument(s)", list.len()-1).to_string())) };
+    let evald_second = eval(&list[2], repl_env)?;
+    let def_sym = if let MalType::Symbol(sym) = &list[1] { sym } else { return Err(MalErr::TypeErr1("symbol".to_string(), list[1].prt_type().to_string(), "argument 1 to def!".to_string())) };
+    repl_env.set(def_sym, evald_second.clone());
+    Ok(evald_second)
+}
+
+fn eval_let(list: &Vec<MalType>, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
+    if list.len() != 3 { return Err(MalErr::ElementErr1("2 arguments for let*".to_string(), format!("{} argument(s)", list.len()-1))) }
+    repl_env.new_env();
+    let result = handle_let_bindings(&list[1], repl_env);
+    if let Err(malerr) = result {
+        repl_env.drop_env();
+        return Err(malerr);
+    }
+    let eval_result = eval(&list[2], repl_env);
+    repl_env.drop_env();
+    eval_result
 }
 
 fn eval(mt: &MalType, repl_env: &mut MalEnv) -> Result<MalType, MalErr> {
     match mt {
-        MalType::List(list) => match list.len() {
-            0 => {
+        MalType::List(list) => {
+            if list.len() == 0 {
                 let new_list: Vec<MalType> = Vec::new();
                 Ok(MalType::List(new_list))
-            }
-            _ => {
+            } else {
                 match &list[0] {
                     MalType::Symbol(sym) => match sym.as_str() {
-                        "def!" => {
-                            match list.len() {
-                                3 => match eval(&list[2], repl_env) {
-                                    Ok(evald_second) => match &list[1] {
-                                        MalType::Symbol(def_sym) => {
-                                            repl_env.set(def_sym, evald_second.clone());
-                                            Ok(evald_second)
-                                        }
-                                        _ => Err(MalErr::TypeErr1("symbol".to_string(), list[1].prt_type().to_string(), "argument 1 to def!".to_string()))
-                                    }
-                                    Err(malerr) => Err(malerr)
-                                }
-                                _ => Err(MalErr::ElementErr1("2 arguments for def!".to_string(), format!("{} argument(s)", list.len()-1).to_string()))
-                            }
-                        }
-                        "let*" => {
-                            match list.len() {
-                                3 => {
-                                    repl_env.new_env();
-                                    match handle_let_bindings(&list[1], repl_env) {
-                                        Err(malerr) => {
-                                            repl_env.drop_env();
-                                            return Err(malerr);
-                                        }
-                                        _ => {}
-                                    }
-                                    match eval(&list[2], repl_env) {
-                                        Ok(result) => {
-                                            repl_env.drop_env();
-                                            Ok(result)
-                                        }
-                                        Err(malerr) => {
-                                            repl_env.drop_env();
-                                            Err(malerr)
-                                        }
-                                    }
-                                }
-                                _ => Err(MalErr::ElementErr1("2 arguments for let*".to_string(), format!("{} argument(s)", list.len()-1)))
-                            }
-                        }
-                        _ => match eval_ast(mt, repl_env) {
-                            Ok(result) => match result {
-                                MalType::List(evald_list) => apply(&evald_list[0], &evald_list[1], &evald_list[2]),
-                                _ => Err(MalErr::InternalErr2("list not returned"))
-                            }
-                            Err(err_str) => Err(err_str)
+                        "def!" => eval_def(list, repl_env),
+                        "let*" => eval_let(list, repl_env),
+                        _ => {
+                            let result = eval_ast(mt, repl_env)?;
+                            let evald_list = if let MalType::List(l) = result { l } else { return Err(MalErr::InternalErr2("list not returned")) };
+                            apply(&evald_list[0], &evald_list[1], &evald_list[2])
                         }
                     }
-                    _ => match eval_ast(mt, repl_env) {
-                        Ok(result) => match result {
-                            MalType::List(evald_list) => apply(&evald_list[0], &evald_list[1], &evald_list[2]),
-                            _ => Err(MalErr::InternalErr2("list not returned"))
-                        }
-                        Err(err_str) => Err(err_str)
+                    _ => {
+                        let result = eval_ast(mt, repl_env)?;
+                        let evald_list = if let MalType::List(l) = result { l } else { return Err(MalErr::InternalErr2("list not returned")) };
+                        apply(&evald_list[0], &evald_list[1], &evald_list[2])
                     }
                 }
             }
@@ -208,13 +141,11 @@ fn read(buffer: String) -> Result<MalType, &'static str> {
 }
 
 fn rep(buffer: String, repl_env: &mut MalEnv) -> Result<String, MalErr> {
-    match read(buffer) {
-        Ok(mt) => match eval(&mt, repl_env) {
-            Ok(result) => Ok(print(result)),
-            Err(malerr) => Err(malerr)
-        }
-        Err(err_str) => Err(MalErr::Generic1(err_str.to_string()))
-    }
+    let read_result = read(buffer);
+    if let Err(err_str) = read_result { return Err(MalErr::Generic1(err_str.to_string())) }
+    let mt = read_result.unwrap();
+    let eval_result = eval(&mt, repl_env)?;
+    Ok(print(eval_result))
 }
 
 fn main() {
@@ -224,19 +155,16 @@ fn main() {
         print!("user> ");
         stdout().flush().unwrap();
         let mut buffer = String::new();
-        match stdin().read_line(&mut buffer) {
-            Ok(n) => {
-                /* Catch a CTRL-d */
-                if n == 0 {
-                    println!("");
-                    break;
-                }
-                match rep(buffer.trim_end().to_string(), &mut repl_env) {
-                    Ok(out_str) => println!("{}", out_str),
-                    Err(malerr) => println!("{}", malerr)
-                }
-            },
-            Err(_) => continue,
+        if let Ok(n) = stdin().read_line(&mut buffer) {
+            /* Catch a CTRL-d */
+            if n == 0 {
+                println!("\nEOF");
+                break;
+            }
+            match rep(buffer.trim_end().to_string(), &mut repl_env) {
+                Ok(out_str) => println!("{}", out_str),
+                Err(malerr) => println!("{}", malerr)
+            }
         }
     }
 }
